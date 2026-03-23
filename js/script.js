e0b80dd7a576568370fa563122bef15fc6fedff7
ilmnayccmm <119484547+ilmnayccmm@users.noreply.github.com>
2026-03-23 12:43:01 +0200

    Практична 1 та 2 готові

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const burger = document.getElementById("burger");
 const nav = document.getElementById("nav");
 
+if(burger){
 burger.addEventListener("click", () => {
-
 nav.classList.toggle("active");
-
 });
+}
